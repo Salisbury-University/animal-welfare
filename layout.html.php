bceea5ff7c9c3b55dbb2274eb188306eb8067b85
Rachel Prusacki <111210220+rprusacki1@users.noreply.github.com>
2023-02-22 23:59:43 -0500

    Layout "stylesheet" for the navigation bar

---
+++ layout.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="public.css">
+    </head>
+    <body>
+        <img src=Images/logo.png alt="Logo">
+        <header><h1></h1> </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="edit.php">Edit Welfare Forms</a></li>
+                <li><a href="diet.php">Diet Tracker</a></li>
+                <li><a href="search.php">Search</a></li>
+            </ul>
+        </nav>
+        <main>
+            <?=$output?>
+        </main>
+        <footer>&copy; GRDJ 2023</footer>
+    </body>
+</html>
