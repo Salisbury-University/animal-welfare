07ed14337a954c51e6acc787a93f941d5b796ef2
feta23 <79985454+feta23@users.noreply.github.com>
2023-03-10 13:05:17 -0500

    Create headerv2.php
    ref to a different style sheet.

---
+++ headerv2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--<link rel="stylesheet" href="public.css"> -->
+        <link rel="stylesheet" type="text/css" href="search.css">
+
+    </head>
+    <body>
+        <img src=Images/salisburyZooLogo.png alt="Logo">
+        <header><h1></h1> </header>
+        <nav>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="edit.php">Edit Welfare Forms</a></li>
+                <li><a href="diet.php">Diet Tracker</a></li>
+                <li><a href="searchv2.php">Search</a></li>
+            </ul>
+        </nav>
+
+            <?=$output?>
+    </body>
+</html>
