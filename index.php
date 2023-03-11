e6478b71b7d8a18d78b3ac45aa62b3f34c4c220d
Cheddar3 <48459112+Cheddar3@users.noreply.github.com>
2023-03-10 20:01:06 -0500

    Implement basic functionality for the login screen
    Still very much a work in progress, but now it will let you log in with the username/password combination of "user1" "pass1" which are stored in the database.

--- index.php
+++ index.php
@@ -9,15 +9,10 @@
 
         <img class="salisburyZooLogoImage" src="Images/salisburyZooLogo.png" />
 
-                <!-- Replace demo_index.php with whatever PHP file is going to handle
-                the login. (SET THIS UP GEORGE, PLS)
-                -->
-        <form class="loginForm" action="demo_index.php" method="get">
-            <input type="text" placeholder="Username" name="user"/>
-            <input type="password" placeholder="Password" name="pwd"/>
-            <br> <br>
+        <form class="loginForm" action="loginHandler.php" method="post">
+            <input type="text" placeholder="Username" name="submittedUsername"/>
+            <input type="password" placeholder="Password" name="submittedPassword"/>
             <input type="submit" value="Log in"/>
         </form>
-
     </body>
 </html>
---
+++ loginHandler.php
@@ -0,0 +1,37 @@
+<?php
+$databaseIP = "localhost";
+$dbusername = "restricted_user";
+$dbpassword = "9jlczTmjCMCdK6Tl";
+$dbName = "test_useraccounts";
+
+session_start();
+
+$connection = mysqli_connect($databaseIP, $dbusername, $dbpassword, $dbName);
+
+function validateData($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$submittedUsername = $_POST['submittedUsername'];
+$submittedPassword = $_POST['submittedPassword'];
+
+$loginUsername = validateData($submittedUsername);
+$loginPassword = validateData($submittedPassword);
+
+echo "Login Username: $loginUsername <br>";
+echo "Login Password: $loginPassword <br>";
+
+$sql = "select * from users where username='$loginUsername' AND password='$loginPassword';";
+$result = mysqli_query($connection, $sql);
+
+$row = mysqli_fetch_assoc($result);
+
+if($row['username'] === $loginUsername && $row['password'] === $loginPassword){
+    echo "Logged in!";
+    $_SESSION['username'] = $row['username'];
+    header("Location: home.php");
+    exit();
+}
