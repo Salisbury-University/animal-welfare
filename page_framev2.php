ba5d968bc0ce478c5b3fbd05a624c7893cdccec5
Josh Brull <106463819+jtrbg@users.noreply.github.com>
2023-03-12 17:04:27 -0400

    Update page_framev2.php

--- page_framev2.php
+++ page_framev2.php
@@ -11,6 +11,7 @@
         <header><h1></h1> </header>
         <nav>
             <ul>
+                <li><a href="home.php">Home</a></li> <!--possible removal-->
                 <li><a href="edit.php">Welfare Forms</a></li>
                 <li><a href="diet.php">Diet Tracker</a></li>
                 <li><a href="searchv2.php">Search</a></li>
