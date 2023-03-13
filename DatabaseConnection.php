f2341665c3b9b1c147b4abd3f4ac45f41ef0cf8d
Rachel Prusacki <111210220+rprusacki1@users.noreply.github.com>
2023-03-13 12:35:08 -0400

    Update DatabaseConnection.php

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,17 +1,16 @@
 <?php
-$servername = 'localhost:3306';
-$username = 'rachel';
-$password = 'Kt0qvUB5QWzAkQqS';
-$dbname = 'Rachel';
+$databaseIP = "localhost";
+$dbusername = "rachel";
+$dbpassword = "Kt0qvUB5QWzAkQqS";
+$dbName = "Rachel";
+
+session_start();
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-##if ($conn)
- ## echo "oo";
+$connection = mysqli_connect($databaseIP, $dbusername, $dbpassword, $dbName);
 
 // Check connection
-if ($conn->connect_error) {
+if ($connection->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
