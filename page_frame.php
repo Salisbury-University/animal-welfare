0150c4ec9949c4bbb10112e254ecbee3cfb35288
David <48459112+Cheddar3@users.noreply.github.com>
2023-03-27 21:19:02 +0000

    Add functioning logout button

--- page_frame.php
+++ page_frame.php
@@ -14,6 +14,7 @@
                 <li><a href="edit.php">Welfare Forms</a></li>
                 <li><a href="diet.php">Diet Tracker</a></li>
                 <li><a href="search.php">Search</a></li>
+                <li><a href="logoutHandler.php">Logout</a></li>
             </ul>
         </nav>
         <main>
