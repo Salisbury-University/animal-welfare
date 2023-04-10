ae2a9e84169c43f239b7d35433fd35396608ddd0
Rachel Prusacki <rachel.prusacki@gmail.com>
2023-04-10 12:20:53 -0400

    more updates

--- entry.php
+++ entry.php
@@ -7,6 +7,7 @@ $formID = 1;
 $post = array();
 $post[0] = array();
 
+
 ##Initializes sections variable with all questions and ids
 $sql = "SELECT title, hsq.id, s.id 
         from sections s 
@@ -17,35 +18,37 @@ $sections = mysqli_query($connection, $sql);
 ##Counts Sections and Initializes questions
 $count = 0;
 while ($row = mysqli_fetch_array($sections)) {
-    echo "<p>". $row["title"] ."</p>";
-    $posts[count][0] = $row;
+    $posts[$count][0] = $row;
     $count++;
 }
-##works^
+
 
 ##Initializes questions variable with all questions and ids
-for ($i = 0; $i < count; $i++) {
+for ($i = 0; $i < $count; $i++) {
+    $x = 1; //Start at 1 to not override sections which are in x=0
     $sql = "SELECT q.question, hsq.id
         from questions q
         join hasSectionQuestions hsq on q.id = hsq.question_id
-        where hsq.section_id = ". $i['id'] ."and hsq.form_id = ". $formId . ";";
+        where hsq.section_id = 1 and hsq.form_id = 1;";
     $questions = mysqli_query($connection, $sql);
-
+    
     #Loads question from section into col1+ of array
-    $x = 1;
-    while ($col = mysqli_fetch_array($sections)) {
-        $post[$i][x] = $col;
+    while ($col = mysqli_fetch_array($questions)) {
+        $posts[$i][$x] = $col;
         $x++;
     }
 }
 
+
 ##Printing
 for ($row = 0; $row <2; $row++) {
-    echo "<p> Row number ".$posts[$row["title"]][0]."</p>";
+    echo "<p> Section: ".$posts[$row][0]["title"]."</p>";
     for ($col = 1; $col < 3; $col++) {
-        echo "<li>".$posts[$row][$col["question"]]."</li>";
+        echo "<li>".$posts[$row][$col]["question"]."</li>";
     }
 }
+
+
 
 #Include html
 ob_start();
