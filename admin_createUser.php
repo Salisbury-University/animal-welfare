d93e81788cf62d1d5d8ffd40e70005127d0a67ad
Cheddar3 <48459112+Cheddar3@users.noreply.github.com>
2023-04-23 17:13:13 +0000

    Password hashing

--- admin_createUser.php
+++ admin_createUser.php
@@ -6,15 +6,14 @@ if($isAdmin == false){
     header('Location: home.php');
 }
 
-echo "admin_createUser.php script <br>";
-
 //TODO: Check if currently logged in user has the admin flag (To prevent unprivileged users from using it)
 $submittedEmail = $_POST['email'];
 $submittedPassword = $_POST['password'];
 $submittedAdminFlag = $_POST['admin'];
 
-echo "DEBUG: '$submittedUsername' - '$submittedEmail' - '$submittedPassword' - '$submittedAdminFlag' <br>";
-$sql="INSERT INTO users(email, pass, administrator) VALUES ('$submittedEmail', '$submittedPassword', $submittedAdminFlag);";
+$hashedPassword = password_hash($submittedPassword, PASSWORD_DEFAULT);
+
+$sql="INSERT INTO users(email, pass, administrator) VALUES ('$submittedEmail', '$hashedPassword', $submittedAdminFlag);";
 $result = mysqli_query($connection, $sql);
 
 // If the query fails, leave the user on the page.
