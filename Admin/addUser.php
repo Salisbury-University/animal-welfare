d564015d93ed3251140e7d2683ace36ee723938f
David <48459112+Cheddar3@users.noreply.github.com>
2023-05-22 12:17:43 -0400

    Fix adduser being an older version of the script

--- Admin/addUser.php
+++ Admin/addUser.php
@@ -2,17 +2,19 @@
 <?php
        include "../Includes/preventUnauthorizedUse.php";
         
-        $submittedEmail = $_POST['email'];
-        $submittedPassword = $_POST['password'];
-        $submittedAdminFlag = $_POST['admin'];
 
-            
-        $sql="INSERT INTO users(email, pass, administrator) VALUES ('$submittedEmail', '$submittedPassword', $submittedAdminFlag);";
-        $result = mysqli_query($connection, $sql);
-        
-            
-        // Redirect to home directory
-        header("Location: ../admin.php");
+       $submittedEmail = $_POST['email'];
+       $submittedPassword = $_POST['password'];
+       $submittedAdminFlag = $_POST['admin'];
+
+       $hashedPassword = password_hash($submittedPassword, PASSWORD_DEFAULT);
+
+       $sql="INSERT INTO users(email, pass, administrator) VALUES ('$submittedEmail', '$hashedPassword', $submittedAdminFlag);";
+       $result = mysqli_query($connection, $sql);
+       
+           
+       // Redirect to home directory
+       header("Location: ../admin.php");
 
         
 ?>
