e1fcf799da833aab62d5751288407e616c610dca
Rachel Prusacki <111210220+rprusacki1@users.noreply.github.com>
2023-09-12 10:01:26 -0400

    Update createAnimal.php
    Added Form Select

--- createAnimal.php
+++ createAnimal.php
@@ -97,20 +97,20 @@ $forms = mysqli_query($connection, $sql);
       <?php
         include "Includes/DatabaseConnection.php"; // Start session
         include "Includes/preventUnauthorizedUse.php";
+        include "loginError.php"; //Displays error if something is NULL
 
-        //Get ID:
-        $zims= $_GET['id'];
-        $sql = 'SELECT * FROM `animals` WHERE `id` = ' . $zims;
-        $q = mysqli_query($connection, $sql);
-        $animal = mysqli_fetch_array($q);
-
+        //Displays Sections:
         $sql = 'SELECT DISTINCT `section` FROM `animals` ORDER BY `section` ASC';
         $q = mysqli_query($connection, $sql);
 
+        //Displays Species:
+        $sql = 'SELECT DISTINCT `id` FROM `species`';
+        $s = mysqli_query($connection, $sql);
   
       ?>
       
         <!--Start HTML-->
+        
     <div class = "my-container" style="border:5px solid #000000;"">
         <h1>Create Animal Form:</h1>
         <!--Start form-->       
@@ -131,20 +131,19 @@ $forms = mysqli_query($connection, $sql);
                   while($sections = mysqli_fetch_array($q)) { 
                 ?>
       
-                   
                 <div class="row">
                   <div class="col">
                     <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="location" id="location">
-                    <label class="form-check-label" for="location"> <?php echo $sections['section']; ?></label>
+                    <input class="form-check-input" type="radio" name="location" id="location"
+                    value= "<?php echo $sections['section']; ?>"> <?php echo $sections['section']; ?>
                   </div>
 
                   <?php if($sections = mysqli_fetch_array($q)) { ?>
                   </div>
                   <div class="col">
                     <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="radio" name="location" id="location">
-                      <label class="form-check-label" for="location"> <?php echo $sections['section']; ?></label>
+                    <input class="form-check-input" type="radio" name="location" id="location"
+                    value= "<?php echo $sections['section']; ?>"> <?php echo $sections['section']; ?>
                     </div>
                   </div>
 
@@ -152,8 +151,8 @@ $forms = mysqli_query($connection, $sql);
         
                   <div class="col">
                     <div class="form-check form-check-inline">
-                      <input class="form-check-input" type="radio" name="location" id="location">
-                      <label class="form-check-label" for="location"> <?php echo $sections['section']; ?></label>
+                    <input class="form-check-input" type="radio" name="location" id="location"
+                    value= "<?php echo $sections['section']; ?>"> <?php echo $sections['section']; ?>
                     </div>
                   </div>
                   <?php } 
@@ -172,10 +171,11 @@ $forms = mysqli_query($connection, $sql);
             </div>
             <!----> 
 
-            <!--Sex Dropdown--> 
+
+            <!--Sex Dropdown add values--> 
             <div class="form-group">
               <label for="sex">Select Sex:</label>
-              <select class="form-control">
+              <select class="form-control" name = "Sex">
               <option value ="">Select..</option>
               <option value="M">Male</option>
               <option value="F">Female</option>
@@ -193,7 +193,22 @@ $forms = mysqli_query($connection, $sql);
             <!--Select Species--> 
             <div class="form-group">
                 <label for="ad">Species:</label>
-                <input type="text" class="form-control" name='ad' placeholder="Enter Species">
+                <input type="text" class="form-control" name='species' placeholder="Enter Species">
+            </div>
+            <!----> 
+
+            <!--Form ID--> 
+            <div class="form-group">
+              <label for="form">Select Form:</label>
+              <select class="form-control" name = "form">
+                <option value ="">Select..</option>
+                <option value="1">Education Bird Welfare Assessment Form</option>
+                <option value="2">Education Mammal Welfare Assessment Form</option>
+                <option value="3">Education Ectotherm Welfare Assessment Form</option>
+                <option value="4">Collection Bird Welfare Assessment Form</option>
+                <option value="5">Collection Mammal Welfare Assessment Form</option>
+                <option value="6">Collection Ectotherm Welfare Assessment Form</option>
+              </select>      
             </div>
             <!----> 
 
