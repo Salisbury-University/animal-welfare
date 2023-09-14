7048a71a0254d22128c428e70529102cfaa134c2
Cheddar3 <48459112+Cheddar3@users.noreply.github.com>
2023-09-14 13:35:03 +0000

    Remove database file dependency for preventUnauthorizedUse.php

--- Includes/preventUnauthorizedUse.php
+++ Includes/preventUnauthorizedUse.php
@@ -1,6 +1,8 @@
 <?php
-include "DatabaseConnection.php";
-include "loggedInUserHelper.php";
+include_once("loggedInUserHelper.php");
+
+if(session_status() != 2) // If session has not started yet, Start it.
+    session_start();
 
 if(checkIsLoggedIn() == false){
     header("Location: index.php");
