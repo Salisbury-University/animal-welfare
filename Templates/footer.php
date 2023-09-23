1c5334112da04e2a6f5408e0569fbd450cae5ed1
Cheddar3 <48459112+Cheddar3@users.noreply.github.com>
2023-09-23 20:17:41 +0000

    Refactor home.php

---
+++ Templates/footer.php
@@ -0,0 +1,42 @@
+<hr>
+    <footer class="container-fluid">
+        <div class="f-top">
+            <div class="row">
+                <div class="col">
+                    <h4>welfare</h4>
+                    <ul>
+                        <li><a href="#">animals</a></li>
+                        <li><a href="#">species</a></li>
+                        <li><a href="#">sections</a></li>
+                        <li><a href="#">checkups</a></li>
+                        <li><a href="#">forms</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>diets</h4>
+                    <ul>
+                        <li><a href="#">coming soon</a></li>
+                        <!-- <li><a href=''></li> -->
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>data</h4>
+                    <ul>
+                        <li><a href="#">compare animals</a></li>
+                        <li><a href="#">view all</a></li>
+                        <li><a href="#">export data</a></li>
+                        <!-- <li><a href="#">interactive map</a></li> -->
+                        <!-- <li><a href=''></li> -->
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>help</h4>
+                    <ul>
+                        <li><a href="help.php">help page</a></li>
+                        <!-- <li><a href=''></li> -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
