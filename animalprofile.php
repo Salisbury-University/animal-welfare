e4acc711216caf28d277d2572611d85d25b535b5
Cheddar3 <48459112+Cheddar3@users.noreply.github.com>
2023-09-23 21:05:14 +0000

    Refactor animalprofile.php

--- animalprofile.php
+++ animalprofile.php
@@ -1,19 +1,12 @@
 <?php
-include "Includes/preventUnauthorizedUse.php";
-
-##Initializes forms variable
-$sql = "SELECT * FROM `forms`;";
-$forms = mysqli_query($connection, $sql);
-
-
+include_once("Templates/header.php");
 
 //Custom for animal profile page
 //Get ID:
 $zims= $_GET['id'];
-
   
-$query = 'SELECT * FROM animals WHERE id= ' . $zims; 
-$r = mysqli_query($connection, $query);
+$query = 'SELECT * FROM animals WHERE id=?'; 
+$r = $database->runParameterizedQuery($query, "i", array($zims));
 $row = mysqli_fetch_array($r);
 $name = $row['name'];
 $species = $row['species_id'];
@@ -24,45 +17,26 @@ $birth=$row['birth_date'];
 $avg = "N/A";
 $lastcheckup = "N/A";
 
-$query = 'SELECT form_id FROM species WHERE id= \'' . $species . '\'';
-$formID = mysqli_query($connection, $query);
+$query = 'SELECT form_id FROM species WHERE id=?';
+$formID = $database->runParameterizedQuery($query, "s", array($species));
 $formID = mysqli_fetch_assoc($formID);
 $formID = $formID['form_id'];
 
-
-
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Dashboard</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="CSS/main.css" rel="stylesheet">
-    <link href="CSS/display.css" rel="stylesheet">
-
-    <!--Boostrap javascript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-
-    <style>
-      .scores{
-        height:40%;
-        overflow-y:auto;
-        
-      }
-      .notebox{
-        height: 45%;
-        overflow-y:auto;
-        border:2px solid black;
-      }
-    </style>
+<link href="CSS/display.css" rel="stylesheet">
+<style>
+  .scores{
+    height:40%;
+    overflow-y:auto;
+    
+  }
+  .notebox{
+    height: 45%;
+    overflow-y:auto;
+    border:2px solid black;
+  }
+</style>
 
 	<!--For search page // this function is not in use-->
 	<script> 
@@ -120,87 +94,11 @@ $formID = $formID['form_id'];
         }
     }
 }
+  </script>
+</head>
 
 
-	</script>
-  </head>
-
-  <body>
-    <!--Header-->
-    <header></header>
-    
-    <!--Navigation Bar-->
-    <hr>
-    <nav class="navbar navbar-expand-md my-light">
-    
-    <!--Logo-->
-    <div class = "logo-overlay">
-      <a href="home.php">
-        <img src=Images/Header/logo.png alt="Logo">
-      </a>
-    </div>
-
-      <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav mr-auto">
-          <!--Home-->
-          <li class="nav-item">
-            <a class="nav-link my-text-info" href="home.php">Home</a>
-          </li>
-
-          <!--Diet Tracker-->
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Diet Tracker</a>
-          </li>
-
-          <!--Search Page-->
-          <li class="nav-item">
-            <a class="nav-link my-text-info" href="search.php">Search</a>
-          </li>
-          
-          <!--Start Admin Only-->
-          <?php
-            $isAdmin = checkIsAdmin();
-            if($isAdmin == true){ ?>
-          
-                    <!--Welfare Forms-->
-                    <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle my-text-info" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Edit Forms
-              </a>
-      
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <?php while ($form = mysqli_fetch_array($forms, MYSQLI_ASSOC)): ?>
-                  <form method="POST" action="Forms/Forms.php?id=<?php echo $form['id']; ?>">
-                    <button type="submit" class="dropdown-item btn btn-secondary"><?php echo $form["title"]; ?></button>
-                  </form>
-                <?php endwhile; ?>
-              </div>
-            </li>
-          
-
-          <!--Dropdown menu-->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle my-text-info" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Admin
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="admin.php">Manage admin</a>
-              <a class="dropdown-item" href="admin_createUser.php">Create User</a>
-            </div>
-          </li>
-
-          <?php } ?> <!--End admin only-->
-        </ul>
-        <a class="btn btn-success my-2 my-sm-0 float-left" href="logoutHandler.php" role="button">Logout</a>
-      
-      </div>
-    </nav>
-    <hr>
-    <!--End Navigation Bar-->
-
-
-    <!--Only edit main-->
-    <main><!-- Main jumbotron for a primary marketing message or call to action -->
+<main><!-- Main jumbotron for a primary marketing message or call to action -->
 		<div class="container-fluid">
 		<div class ="row">
 
@@ -214,7 +112,7 @@ $formID = $formID['form_id'];
 
       <?php 
         $sql = "SELECT MAX(dates) AS lastcheckup FROM welfaresubmission";
-        $result = mysqli_query($connection, $sql);
+        $result = $database->runQuery_UNSAFE($sql);
         $row = mysqli_fetch_assoc($result);
         $lastcheckup = $row['lastcheckup'];
       ?>
@@ -241,12 +139,9 @@ $formID = $formID['form_id'];
           
           $stmt = "SELECT wid, dates, reason, avg_health, avg_nutrition, avg_pse, avg_behavior, avg_mental 
                     FROM welfaresubmission 
-                    WHERE zim = $zims"; 
-          
+                    WHERE zim = ?"; 
 
-
-
-          $result = mysqli_query($connection, $stmt);
+          $result = $database->runParameterizedQuery($stmt, "d", array($zims));
           
           
           while($row = mysqli_fetch_array($result)){
@@ -265,7 +160,6 @@ $formID = $formID['form_id'];
             echo '<a href="displayentry2.php?id=' . $row['wid'] . '">Entry #' . $row['wid'] . '</a> | ' . $row['dates'] . ' | ' . $row['reason'] . ' | Entry Average: ' . number_format($average, 2) . '<br>';
 
           }
-          mysqli_close($connection);
         ?>
 					
 				</div>
@@ -293,46 +187,6 @@ $formID = $formID['form_id'];
 		</div> <!--Closes whole row-->
 		</div> <!--Closes whole profile container-->
     </main>
-
-    <hr>
-    <footer class="container-fluid">
-        <div class="f-top">
-            <div class="row">
-                <div class="col">
-                    <h4>welfare</h4>
-                    <ul>
-                        <li><a href="#">animals</a></li>
-                        <li><a href="#">species</a></li>
-                        <li><a href="#">sections</a></li>
-                        <li><a href="#">checkups</a></li>
-                        <li><a href="#">forms</a></li>
-                    </ul>
-                </div>
-                <div class="col">
-                    <h4>diets</h4>
-                    <ul>
-                        <li><a href="#">coming soon</a></li>
-                        <!-- <li><a href=''></li> -->
-                    </ul>
-                </div>
-                <div class="col">
-                    <h4>data</h4>
-                    <ul>
-                        <li><a href="#">compare animals</a></li>
-                        <li><a href="#">view all</a></li>
-                        <li><a href="#">export data</a></li>
-                        <!-- <li><a href="#">interactive map</a></li> -->
-                        <!-- <li><a href=''></li> -->
-                    </ul>
-                </div>
-                <div class="col">
-                    <h4>help</h4>
-                    <ul>
-                        <li><a href="help.php">help page</a></li>
-                        <!-- <li><a href=''></li> -->
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-</body>
+<?php
+include_once("Templates/footer.php");
+?>
