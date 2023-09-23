1c5b78d811d51216eea0a84ef646e1b4c8ef9e6a
Cheddar3 <48459112+Cheddar3@users.noreply.github.com>
2023-09-23 20:55:18 +0000

    Refactor createAnimal.php

--- createAnimal.php
+++ createAnimal.php
@@ -1,111 +1,21 @@
 <?php
-##Initializes forms variable for header
-$sql = "SELECT * FROM `forms`;";
-$forms = mysqli_query($connection, $sql);
+include_once("Templates/header.php");
 ?>
 
-<!doctype html>
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Dashboard</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="CSS/main.css" rel="stylesheet">
-    <link href="CSS/admin.css" rel="stylesheet">
-
-    <!--Boostrap javascript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-
-
-  </head>
-
-  <body>
-    <!--Header-->
-    <header></header>
-    
-    <!--Navigation Bar-->
-    <hr>
-    <nav class="navbar navbar-expand-md my-light">
-    
-    <!--Logo-->
-    <div class = "logo-overlay">
-      <a href="home.php">
-        <img src=Images/Header/logo.png alt="Logo">
-      </a>
-    </div>
-
-      <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav mr-auto">
-          <!--Home-->
-          <li class="nav-item">
-            <a class="nav-link my-text-info" href="home.php">Home</a>
-          </li>
-
-          <!--Diet Tracker-->
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Diet Tracker</a>
-          </li>
-
-          <!--Search Page-->
-          <li class="nav-item">
-            <a class="nav-link my-text-info" href="search.php">Search</a>
-          </li>
-
-          <!--Welfare Forms-->
-          <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle my-text-info" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Edit Forms
-              </a>
-      
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <?php while ($form = mysqli_fetch_array($forms, MYSQLI_ASSOC)): ?>
-                  <form method="POST" action="Forms/Forms.php?id=<?php echo $form['id']; ?>">
-                    <button type="submit" class="dropdown-item btn btn-secondary"><?php echo $form["title"]; ?></button>
-                  </form>
-                <?php endwhile; ?>
-              </div>
-            </li>
-
-          <!--Dropdown menu-->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle my-text-info" href="#" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Admin
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="admin.php">Manage admin</a>
-              <a class="dropdown-item" href="admin_createUser.php">Create User</a>
-            </div>
-          </li>
-        </ul>
-        <a class="btn btn-success my-2 my-sm-0 float-left" href="logoutHandler.php" role="button">Logout</a>
-      
-      </div>
-    </nav>
-    <hr>
-    <!--End Navigation Bar-->
-
+<link href="CSS/admin.css" rel="stylesheet">
 
     <!--Only edit main-->
     <main>
       <?php
-        include_once("Includes/DatabaseConnection.php");
-        include_once("Includes/preventUnauthorizedUse.php");
-        include "loginError.php"; //Displays error if something is NULL
+        include_once("loginError.php"); //Displays error if something is NULL
 
         //Displays Sections:
         $sql = 'SELECT DISTINCT `section` FROM `animals` ORDER BY `section` ASC';
-        $q = mysqli_query($connection, $sql);
+        $q = $database->runQuery_UNSAFE($sql);
 
         //Displays Species:
         $sql = 'SELECT DISTINCT `id` FROM `species`';
-        $s = mysqli_query($connection, $sql);
+        $s = $database->runQuery_UNSAFE($sql);
   
       ?>
       
@@ -235,45 +145,6 @@ $forms = mysqli_query($connection, $sql);
 
     </main>
 
-    <hr>
-    <footer class="container-fluid">
-        <div class="f-top">
-            <div class="row">
-                <div class="col">
-                    <h4>welfare</h4>
-                    <ul>
-                        <li><a href="#">animals</a></li>
-                        <li><a href="#">species</a></li>
-                        <li><a href="#">sections</a></li>
-                        <li><a href="#">checkups</a></li>
-                        <li><a href="#">forms</a></li>
-                    </ul>
-                </div>
-                <div class="col">
-                    <h4>diets</h4>
-                    <ul>
-                        <li><a href="#">coming soon</a></li>
-                        <!-- <li><a href=''></li> -->
-                    </ul>
-                </div>
-                <div class="col">
-                    <h4>data</h4>
-                    <ul>
-                        <li><a href="#">compare animals</a></li>
-                        <li><a href="#">view all</a></li>
-                        <li><a href="#">export data</a></li>
-                        <!-- <li><a href="#">interactive map</a></li> -->
-                        <!-- <li><a href=''></li> -->
-                    </ul>
-                </div>
-                <div class="col">
-                    <h4>help</h4>
-                    <ul>
-                        <li><a href="help.php">help page</a></li>
-                        <!-- <li><a href=''></li> -->
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-</body>
+<?php
+include_once("Templates/footer.php");
+?>
