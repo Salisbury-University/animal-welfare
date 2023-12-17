df7f12596e24aea15d95462d4d36c66fae4ffb27
Cheddar3 <48459112+Cheddar3@users.noreply.github.com>
2023-12-17 18:32:53 +0000

    Finalize admin UI

--- ui/admin.php
+++ ui/admin.php
@@ -24,18 +24,25 @@ $user->openDatabase();
 
 
     // Display table of users
-    $sql = "SELECT email, pass, administrator FROM `users`";
+    $sql = "SELECT email, administrator FROM `users`";
     $result = $user->getDatabase()->runQuery_UNSAFE($sql);
     if ($result->num_rows > 0) {
         ?>
         <h1 class="text-center"><u>All Users:</u></h1>
+
+        <style>
+            .container{
+                margin-left: auto;
+                margin-right: auto;
+            }
+        </style>
 
         <div class="container">
             <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th> Email </th>
-                        <th> Pass </th>
+                        <!-- <th> Pass </th> -->
                         <th> Is_admin </th>
                     </tr>
                 </thead>
@@ -45,15 +52,12 @@ $user->openDatabase();
                     <tbody>
                         <tr>
                             <td><?= htmlspecialchars($row['email'], ENT_QUOTES, 'UTF-8') ?></td>
-                            <td><?= htmlspecialchars($row['pass'], ENT_QUOTES, 'UTF-8') ?></td>
                             <td><?= htmlspecialchars($row['administrator'], ENT_QUOTES, 'UTF-8') ?></td>
                             <td>
                                 <form action="modifyUser.php" method="post">
                                     <input type="hidden" name="email" value="<?= $row['email'] ?>">
                                     <button type="submit" class="btn btn-dark">Modify</button>
-                            </td>
-                            </form>
-                            <td>
+                                </form>
                                 <form action="../admin/_delete_user.php" method="post">
                                     <input type="hidden" name="email" value="<?= $row['email'] ?>">
                                     <button type="submit" class="btn btn-danger">Delete</button>
